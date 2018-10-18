cd53cbe624c7878535987a3ffb95d4c3d3d2689f
Paul Fitzpatrick <paul.michael.fitzpatrick@gmail.com>
2018-10-17 22:02:26 -0400

    functional popup links, but so painful

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="map fade" id="map" onclick="console.log('harro');">
+    <div class="map fade" id="map">
     </div>
   </div>
 </template>
@@ -13,7 +13,16 @@ const L = (window as any).L;
 
 let map: any = null;
 
-function go(base, ndata: any[]|null) {
+function escapeHtml(unsafe: string) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
+
+function go(base: any, ndata: any[]|null) {
   console.log("HELLO", (document as any).getElementById("map"));
   const data = ndata || [
     { latitude: 60, longitude: 50, name: 'zing1' },
@@ -41,7 +50,17 @@ function go(base, ndata: any[]|null) {
     const title = data[i].name;
     const marker = L.marker(pt, { title });
     points.push(pt);
-    marker.bindPopup(title);
+    // oh gosh this is so painful - there must be an easier way to set up a link programmatically.
+    marker.bindPopup("<a class='poplink' data-id='" + data[i].id + "' href='#'>" + escapeHtml(title) + "</a>")
+      .on('popupopen', function (popup: any) {
+        const classname = document.getElementsByClassName("poplink");
+        var myFunction = function(this: any) {
+          base.$router.push('/org/' + this.getAttribute('data-id') + "#");
+        };
+        for (let i = 0; i < classname.length; i++) {
+          classname[i].addEventListener('click', myFunction, false);
+        }
+      });
     markers.addLayer(marker);
   }
   map.addLayer(markers);
